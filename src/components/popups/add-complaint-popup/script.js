87604003d4e4d41cfd98724e88f204c7c60011c0
Vladimir Dolganov <russiatunbay@gmail.com>
2018-07-15 11:32:37 +0700

    pre

--- src/components/popups/add-complaint-popup/script.js
+++ src/components/popups/add-complaint-popup/script.js
@@ -1,8 +1,12 @@
+import axios from 'axios'
 export default {
   name: "add-complaint-popup",
   data() {
     return {
-
+       popTitle: "",
+       popDesc: "",
+       popImage: {},
+       popAdress: ""
     }
   },
   created() {
@@ -10,5 +14,33 @@ export default {
   },
   methods: {
 
+    emitClose(){
+      this.$emit('close')
+    },
+    loadImage(e){
+     this.popImage =  e.target.files[0];
+     console.log(this.popImage);
+
+    },
+    postData(){
+      let data = {
+        address: this.popAdress,
+        title: this.popTitle,
+        description: this.popDesc,
+      };
+      axios.post('http://192.168.88.229:8000/api/citizen/citizen/', data)
+        .then(
+          response => {
+            console.log(response.data);
+            alert("Отправлена заявка : Адрес - " + this.popAdress + ", Заголовок - " + this.popTitle + ", Описание - " + this.popDesc);
+            this.$emit('close')
+          }
+        )
+        .catch(
+          function (error) {
+          console.log(error)
+          }
+        )
+    }
   }
 }
--- src/components/search-panel/script.js
+++ src/components/search-panel/script.js
@@ -1,11 +1,17 @@
+import addComplaintPopup from '../popups/add-complaint-popup/add-complaint-popup'
+
 export default {
   name: "search-panel",
   props: {
 
   },
+
+  components:{
+    addComplaintPopup
+  },
   data() {
     return {
-
+    isAdd: false,
     }
   },
   created() {
